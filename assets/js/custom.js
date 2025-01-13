936a6d35a1e8cff91bd1acd13af68d1a210a0957
lorensansol <lorensansol@gmail.com>
2025-01-13 16:37:45 +0100

    Update: 2025-01-13 16:37:45

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,24 +12,24 @@ scrollShot({
   }
 })
 
-// Maintain GA4 key events
-// contact_click
-document.addEventListener('click', e => {
-  const b = e.target.closest(
-    '.callnow__phone,' +
-    '.callnow__whatsapp,' +
-    '.contact__phone,' +
-    '.contact__whatsapp,' +
-    '.contact__email-option-copy,' +
-    '.contact__email-option-send,' +
-    '.contact__address,' +
-    '.ga4'
-  )
-  // eslint-disable-next-line
-  if (b) gtag('event', 'contact_click')
-})
-// contact_form_submit
-document.addEventListener('submited-form-contacto', e => {
-  // eslint-disable-next-line
-  gtag('event', 'contact_form_submit')
-})
+// // Maintain GA4 key events
+// // contact_click
+// document.addEventListener('click', e => {
+//   const b = e.target.closest(
+//     '.callnow__phone,' +
+//     '.callnow__whatsapp,' +
+//     '.contact__phone,' +
+//     '.contact__whatsapp,' +
+//     '.contact__email-option-copy,' +
+//     '.contact__email-option-send,' +
+//     '.contact__address,' +
+//     '.ga4'
+//   )
+//   // eslint-disable-next-line
+//   if (b) gtag('event', 'contact_click')
+// })
+// // contact_form_submit
+// document.addEventListener('submited-form-contacto', e => {
+//   // eslint-disable-next-line
+//   gtag('event', 'contact_form_submit')
+// })
